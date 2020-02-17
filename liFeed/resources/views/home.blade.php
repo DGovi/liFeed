eee32ec06bb5745a63983c3b340741ee734ca6a9
Daniel Gauvin <danielgauvin01@gmail.com>
2020-02-16 21:17:11 -0500

    Laravel base installed
    changes to default laravel website:
    *added authorization
    *added home page
    *changed fonts

---
+++ liFeed/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    	<div class = "col-3 p-5">
+    		<img src="https://instagram.fymy1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/84064213_555267885069521_5343758430911856640_n.jpg?_nc_ht=instagram.fymy1-1.fna.fbcdn.net&_nc_ohc=lf95ktFeAWkAX_7Hu8P&oh=5161a523452f593b3d6b0d3066e7ca4b&oe=5EE3D930" class="rounded-circle" alt = "profile pic">
+    	</div>
+        <div class= "col-9 pt-5">
+            <div><h1>spidermandaniel</h1></div>
+            <div class="d-flex">
+                <div class = "pr-5"><strong>13</strong> posts</div>
+                <div class = "pr-5"><strong>245</strong> followers</div>
+                <div class = "pr-5"><strong>262</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
