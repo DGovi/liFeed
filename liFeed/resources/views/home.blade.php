119ad77a153ea176f5b7dfee46028da426655273
Daniel Gauvin <danielgauvin01@gmail.com>
2020-02-16 22:54:04 -0500

    added some picture to a profile
    the main things on a profile page

--- liFeed/resources/views/home.blade.php
+++ liFeed/resources/views/home.blade.php
@@ -13,6 +13,22 @@
                 <div class = "pr-5"><strong>245</strong> followers</div>
                 <div class = "pr-5"><strong>262</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold"> Daniel Gauvin</div>
+            <div> 🇨🇦 🇨🇳 | 22| Montreal | ConU | 7'3"maybe</div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="https://instagram.fymy1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/67832861_2389499807960825_2968399544299214420_n.jpg?_nc_ht=instagram.fymy1-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=2_HRVQ1cPJQAX8knk__&oh=f89de54e10e048da9c2fb4fc29bf3dc5&oe=5EC768A7"  class = "w-100" >
+
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fymy1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/67832861_2389499807960825_2968399544299214420_n.jpg?_nc_ht=instagram.fymy1-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=2_HRVQ1cPJQAX8knk__&oh=f89de54e10e048da9c2fb4fc29bf3dc5&oe=5EC768A7" class = "w-100" >
+
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fymy1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/67832861_2389499807960825_2968399544299214420_n.jpg?_nc_ht=instagram.fymy1-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=2_HRVQ1cPJQAX8knk__&oh=f89de54e10e048da9c2fb4fc29bf3dc5&oe=5EC768A7"  class = "w-100">
+
+            </div>
         </div>
     </div>
 </div>
