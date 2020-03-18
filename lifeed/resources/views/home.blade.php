65b8d16543eaeca17b9b4d4fc9b687ec7284743a
Daniel Gauvin <danielgauvin01@gmail.com>
2020-03-18 00:45:54 -0400

    added profile picture and a bit stuff to front end

--- lifeed/resources/views/home.blade.php
+++ lifeed/resources/views/home.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fymy1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/87576561_489363241743552_3338024114022514688_n.jpg?_nc_ht=instagram.fymy1-1.fna.fbcdn.net&_nc_ohc=wPoO3_eBUScAX936gUI&oh=3fc4331ddbf2565fb9d2e3036d9b6249&oe=5E9B9A0F" class="rounded-circle">
+        </div>
+        <div class="row-9 pt-5">
+            <div><h1>spiderdan</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>some number</strong> posts</div>
+                <div class="pr-5"><strong> sme number</strong> followers</div>
+                <div class="pr-5"><strong>some number</strong>following</div>
             </div>
         </div>
     </div>
+
 </div>
 @endsection
