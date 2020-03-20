c908a337e39af3b4b1559993ae8405c8368e9d36
Daniel Gauvin <danielgauvin01@gmail.com>
2020-03-20 00:05:33 -0400

    front end profiel fix

--- liFeed/resources/views/profiles/index.blade.php
+++ liFeed/resources/views/profiles/index.blade.php
@@ -24,7 +24,7 @@
     </div>
     <div class="row pt-4 ">
         @foreach($user->posts as $post)
-            <div class="col-4 pb-4ts">
+            <div class="col-4 pb-4">
                 <img src="/storage/{{ $post->image }}" class="w-100">
             </div>
         @endforeach
