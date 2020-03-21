98a5417e553f12a4c50bdd604f7f65bb14bfa24a
Daniel Gauvin <danielgauvin01@gmail.com>
2020-03-20 23:02:36 -0400

    image path errorsfixed

--- liFeed/app/Profile.php
+++ liFeed/app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : 'profile/yIXsv9Xsu50wWNREPfjDk4w0P9PVMi0cx0nsc971.jpeg';
+        return '/storage/' . $imagePath;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
