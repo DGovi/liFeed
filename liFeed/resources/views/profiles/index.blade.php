cf581a9e4ef82d3117018be20cee90ec9077b85f
Daniel Gauvin <danielgauvin01@gmail.com>
2020-03-21 21:01:31 -0400

    installed telescope

--- liFeed/resources/views/profiles/index.blade.php
+++ liFeed/resources/views/profiles/index.blade.php
@@ -27,8 +27,8 @@
 
                 <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong> {{ $user->profile ->followers->count() }}</strong> followers</div>
-                <div class="pr-5"><strong>some number</strong>following</div>
+                <div class="pr-5"><strong> {{ $user->profile ->followers->count() }}</strong> dumbass followers following</div>
+                <div class="pr-5"><strong> {{$user->following ->count()}}</strong> following</div>
             </div>
             <div class="pt-4"><strong>{{ $user->profile->title }}</strong></div>
             <div>{{ $user->profile->description }}</div>
